31774ea72edea78c941eec0baff61c9ced4ea2f7
Jai Jain <jainjai1080@gmail.com>
2017-09-21 19:56:21 +0530

    ChatApp
    Ionic 3 chat application

---
+++ src/app/app.firebaseconfig.ts
@@ -0,0 +1,9 @@
+var config = {
+apiKey: "AIzaSyA7a5zrFb5eDiK5XKKpFPnq6wcVEJTw1u4",
+authDomain: "chatapp-b746c.firebaseapp.com",
+databaseURL: "https://chatapp-b746c.firebaseio.com",
+projectId: "chatapp-b746c",
+storageBucket: "chatapp-b746c.appspot.com",
+messagingSenderId: "140345426917"
+};
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,56 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import {config} from './app.firebaseconfig';
-import {AngularFireAuth} from 'angularfire2/auth';
-import {AngularFireModule} from 'angularfire2';
+import { File } from '@ionic-native/file';
+import { FileChooser } from '@ionic-native/file-chooser';
+import { FilePath } from '@ionic-native/file-path';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
 import { MyApp } from './app.component';
+import { AuthProvider } from '../providers/auth/auth';
+import { UserProvider } from '../providers/user/user';
+import { ImghandlerProvider } from '../providers/imghandler/imghandler';
+import { RequestsProvider } from '../providers/requests/requests';
+import { ChatProvider } from '../providers/chat/chat';
+import { GroupsProvider } from '../providers/groups/groups';
 
+var config = {
+  apiKey: "AIzaSyA7a5zrFb5eDiK5XKKpFPnq6wcVEJTw1u4",
+  authDomain: "chatapp-b746c.firebaseapp.com",
+  databaseURL: "https://chatapp-b746c.firebaseio.com",
+  projectId: "chatapp-b746c",
+  storageBucket: "chatapp-b746c.appspot.com",
+  messagingSenderId: "140345426917"
+  };
+  var database = firebase.database();
 @NgModule({
   declarations: [
     MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp, {tabsPlacement: 'top'}),
     AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp 
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    File,
+    FilePath,
+    FileChooser,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider,
+    AngularFireAuth,
+    UserProvider,
+    ImghandlerProvider,
+    UserProvider,
+    RequestsProvider,
+    ChatProvider,
+    GroupsProvider
   ]
 })
 export class AppModule {}
